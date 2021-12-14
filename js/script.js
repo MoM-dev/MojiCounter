1608ea235ee1c152e0d724946e8c007250c9d751
parsecmonkey <60334704+parsecmonkey@users.noreply.github.com>
2021-12-15 04:31:18 +0900

    replaceText
    テキストの改行、半角、全角スペースをすべて置換

--- js/script.js
+++ js/script.js
@@ -2,10 +2,19 @@ let textBox = document.getElementById('TextBox');
 let msg = document.getElementById('msg');
 let checkButton = document.getElementById('checkButton');
 
+// テキストの改行、半角、全角スペースをすべて置換
+function replaceText(textValue) {
+    textValue = textValue.replace(/\n/g, ""); // 改行を全て置換
+    textValue = textValue.replace(/ /g, "");  // 半角スペースを全て置換
+    textValue = textValue.replace(/　/g, ""); // 全角スペースを全て置換
+    return textValue;
+}
+
 // ボタンクリック時の処理
 function buttonClick() {
     let textLeng = 0;
-    textLeng = textBox.value.trim().length; // 空白を除くテキストの文字数
+    textValue = replaceText(textBox.value);
+    textLeng = textValue.length; // テキストの文字数
 
     msg.innerText = '文字数：' + textLeng;
 }
